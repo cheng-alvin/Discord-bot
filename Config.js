3dc8883c43549455a18596cf96d6f8751da4cff6
Alvincheng137 <chengalvin333@gmail.com>
2021-09-03 11:17:13 +1000

    Heroku deploy

---
+++ Config.js
@@ -0,0 +1,6 @@
+//Config and token variables - !!DO NOT COMMIT!!
+const config = {
+  token: "ODgyNzc5NDA2Nzk4NTU3MTg0.YTAWYw.CkFeMNryJ7y8YL0dJoin3pfGwwA",
+};
+//Export config
+export default config;
